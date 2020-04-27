87257c5266c7c4a42e319bbdada887eb37698358
Musharraf Alam <61291712+thecoffeeandcode@users.noreply.github.com>
2020-04-27 22:08:09 +0530

    ObjectPoolingManager.cs
    Object pooling script for memory optimisation

---
+++ ObjectPoolingManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolingManager : MonoBehaviour
+{
+    private static ObjectPoolingManager instance;
+    public static ObjectPoolingManager Instance { get { return instance; } }
+    public GameObject bulletPrefab;
+    public int bulletAmount = 20;
+    private List<GameObject> bullets;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+        bullets = new List<GameObject>(bulletAmount);
+        for (int i = 0; i < bulletAmount; i++)
+        {
+            GameObject prefabInstance = Instantiate(bulletPrefab);
+            prefabInstance.transform.SetParent(transform);
+            prefabInstance.SetActive(false);
+            bullets.Add(prefabInstance);
+        }
+        
+    }
+
+    // Update is called once per frame
+    public GameObject GetBullet(bool shotByPlayer)
+    {
+        foreach (GameObject bullet in bullets)
+        {
+            if (!bullet.activeInHierarchy)
+            {
+                bullet.SetActive(true);
+                //whenever we get the reference to the bullet
+                bullet.GetComponent<bullets>().ShotByPlayer=shotByPlayer;
+                return bullet;
+            }
+        }
+        GameObject prefabInstance = Instantiate(bulletPrefab);
+        prefabInstance.transform.SetParent(transform);
+        //we are going to use prefab instance instead of bullet
+        prefabInstance.GetComponent<bullets>().ShotByPlayer = shotByPlayer;
+        bullets.Add(prefabInstance);
+
+        return prefabInstance;
+    }
+        
+}
