eb7a2d19450d4d22d0927576fefa0a70aaea6a7f
Musharraf Alam <61291712+thecoffeeandcode@users.noreply.github.com>
2020-04-27 22:07:12 +0530

    MenuController.cs
    Main Menu script for the game

---
+++ MenuController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    //to load scene level 1
+    public void OnPlay()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void Awake()
+    {
+        //to show the cursor after first person Controller
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
