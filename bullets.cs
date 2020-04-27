f6019c36b336b6a55a05ea0eb47dab0ccc829845
Musharraf Alam <61291712+thecoffeeandcode@users.noreply.github.com>
2020-04-27 22:02:18 +0530

    bullet.cs
    FPS Game Bullet shooting script

---
+++ bullets.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullets : MonoBehaviour
+{
+    public float speed = 800f;
+    public float lifeDuration = 10f;
+    public int damage = 5;
+    private float lifeTimer;
+    // Start is called before the first frame update
+    //the bullets from the player
+    private bool shotByPlayer;
+    public bool ShotByPlayer { get { return shotByPlayer; } set { shotByPlayer = value; } }
+
+    void OnEnable()
+    {
+        lifeTimer = lifeDuration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //make the bullet move
+        transform.position += transform.forward * speed * Time.deltaTime;
+        //check if the bullet should be destroyed
+        lifeTimer -= Time.deltaTime;
+        if (lifeTimer <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
